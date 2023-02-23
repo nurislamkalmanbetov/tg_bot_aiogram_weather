0ef6937176de6529fea78fe10d559feacda34493
nurislamkalmanbetov <nurislam_96@mail.ru>
2023-02-23 19:27:16 +0600

    changes the functions to classes

--- main.py
+++ main.py
@@ -4,54 +4,65 @@ from pprint import pprint
 from config import open_weather_token
 
 
-def get_weather(city, open_weather_token):
-    code_to_smile = {
-        "Clear": "Ясно \U00002600",
-        "Clouds": "Облачно \U00002601",
-        "Rain": "Дождь \U00002614",
-        "Drizzle": "Дождь \U00002614",
-        "Thunderstorm": "Гроза \U000026A1",
-        "Snow": "Снег \U0001F328",
-        "Mist": "Туман \U0001F32B"
-    }
-    try:
-        r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-        )
-        data = r.json()
-        pprint(data)
+class Weather:
+    def __init__(self, city, token):
+        self.city = city
+        self.token = token
+        self.code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
 
-        city = data["name"]
-        cur_weather = data["main"]["temp"]
+    def get_weather_data(self):
+        try:
+            r = requests.get(
+                f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.token}&units=metric"
+            )
+            data = r.json()
+            pprint(data)
 
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
-        else:
-            wd = "Посмотри в окно, не пойму что там происходит"
+            self.cur_weather = data["main"]["temp"]
 
-        humidity = data["main"]["humidity"]
-        pressure = data["main"]["pressure"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        lenght_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-        
+            self.weather_description = data["weather"][0]["main"]
+            if self.weather_description in self.code_to_smile:
+                self.wd = self.code_to_smile[self.weather_description]
+            else:
+                self.wd = "Посмотри в окно, не пойму что там происходит"
+
+            self.humidity = data["main"]["humidity"]
+            self.pressure = data["main"]["pressure"]
+            self.wind = data["wind"]["speed"]
+            self.sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+            self.sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+            self.lenght_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+                data["sys"]["sunrise"])
+
+        except Exception as ex:
+            print(ex)
+            print("Проверьте название города: ")
+
+    def display_weather(self):
         print(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-              f"погода в городе: {city}\nТемпература: {cur_weather}C°{wd}\n"
-              f"Влажнось: {humidity}\т Давление: {pressure} мм.рт.ст\nВетер: {wind} метр в секунду\n"
-              f"Восход солнца: {sunrise_timestamp}\nПродолжительность дня: {lenght_of_the_day}\n"
+              f"погода в городе: {self.city}\nТемпература: {self.cur_weather}C°{self.wd}\n"
+              f"Влажнось: {self.humidity}\т Давление: {self.pressure} мм.рт.ст\nВетер: {self.wind} метр в секунду\n"
+              f"Восход солнца: {self.sunrise_timestamp}\nПродолжительность дня: {self.lenght_of_the_day}\n"
               f"Хорошего дня!")
-    except Exception as ex:
-        print(ex)
-        print("Проверьте название города: ")
+
+    def get_weather(self):
+        self.get_weather_data()
+        self.display_weather()
 
 
 def main():
     city = input("Введие город: ")
-    get_weather(city, open_weather_token)
+    weather = Weather(city, open_weather_token)
+    weather.get_weather()
 
 
-if __name__=="__main__":              
-    main() 
+if __name__ == "__main__":
+    main()
--- main_weather_bot_tg.py
+++ main_weather_bot_tg.py
@@ -1,65 +1,71 @@
-import requests
 import datetime
-from config import tg_bot_token, open_weather_token
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+import requests
+from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+from config import tg_bot_token, open_weather_token
 
 
-bot = Bot(token=tg_bot_token)
-dp = Dispatcher(bot)
+class WeatherBot:
+    def __init__(self, tg_bot_token, open_weather_token):
+        self.bot = Bot(token=tg_bot_token)
+        self.dp = Dispatcher(self.bot)
+        self.open_weather_token = open_weather_token
+        self.code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
 
+    async def start_command(self, message: types.Message):
+        await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
 
-@dp.message_handler(commands=["start"])
-async def start_command(message: types.Message):
-    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
+    async def get_weather(self, message: types.Message):
+        try:
+            r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={message.text}"
+                             f"&appid={self.open_weather_token}&units=metric")
+            data = r.json()
 
+            city = data["name"]
+            cur_weather = data["main"]["temp"]
 
-@dp.message_handler()
-async def get_weather(message: types.Message):
-    code_to_smile = {
-        "Clear": "Ясно \U00002600",
-        "Clouds": "Облачно \U00002601",
-        "Rain": "Дождь \U00002614",
-        "Drizzle": "Дождь \U00002614",
-        "Thunderstorm": "Гроза \U000026A1",
-        "Snow": "Снег \U0001F328",
-        "Mist": "Туман \U0001F32B"
-    }
+            weather_description = data["weather"][0]["main"]
+            if weather_description in self.code_to_smile:
+                wd = self.code_to_smile[weather_description]
+            else:
+                wd = "Посмотри в окно, не пойму что там за погода!"
 
-    try:
-        r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
-        )
-        data = r.json()
+            humidity = data["main"]["humidity"]
+            pressure = data["main"]["pressure"]
+            wind = data["wind"]["speed"]
+            sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+            sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+            length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+                data["sys"]["sunrise"])
 
-        city = data["name"]
-        cur_weather = data["main"]["temp"]
+            await message.reply(
+                f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+                f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+                f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+                f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\n"
+                f"Продолжительность дня: {length_of_the_day}\n"
+                f"***Хорошего дня!***"
+            )
+        except:
+            await message.reply("\U00002620 Проверьте название города \U00002620")
 
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
-        else:
-            wd = "Посмотри в окно, не пойму что там за погода!"
+    def register_handlers(self):
+        self.dp.register_message_handler(self.start_command, commands=["start"])
+        self.dp.register_message_handler(self.get_weather)
 
-        humidity = data["main"]["humidity"]
-        pressure = data["main"]["pressure"]
-        wind = data["wind"]["speed"]
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
-        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
-            data["sys"]["sunrise"])
-
-        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
-              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
-              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-              f"***Хорошего дня!***"
-              )
-
-    except:
-        await message.reply("\U00002620 Проверьте название города \U00002620")
+    def start(self):
+        executor.start_polling(self.dp)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    bot = WeatherBot(tg_bot_token, open_weather_token)
+    bot.register_handlers()
+    bot.start()
